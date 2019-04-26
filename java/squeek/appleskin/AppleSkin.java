799e65a1951c68f4a84dc45a69315e964039ca1e
Raph Hennessy <raph.hennessy@gmail.com>
2019-04-26 18:50:29 +1000

    tried to update but ultimately failed because loader br0k3e

--- java/squeek/appleskin/AppleSkin.java
+++ java/squeek/appleskin/AppleSkin.java
@@ -1,13 +1,16 @@
 package squeek.appleskin;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
 import squeek.appleskin.network.SyncHandler;
 
-public class AppleSkin implements ModInitializer
+public class AppleSkin implements ClientModInitializer
 {
 	@Override
-	public void onInitialize()
+	public void onInitializeClient()
 	{
+		System.out.println("class loader class name " + getClass().getClassLoader().getClass().getName());
 		SyncHandler.init();
 	}
 }
--- java/squeek/appleskin/mixin/MinecraftClientMixin.java
+++ java/squeek/appleskin/mixin/MinecraftClientMixin.java
@@ -1,15 +1,22 @@
 package squeek.appleskin.mixin;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.RunArgs;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import squeek.appleskin.client.HUDOverlayHandler;
+import squeek.appleskin.network.SyncHandler;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin
 {
+	@Inject(at = @At("RETURN"), method="init")
+	private void init(CallbackInfo info) {
+		//SyncHandler.init();
+	}
+
 	@Inject(at = @At("HEAD"), method = "tick")
 	void onTick(CallbackInfo info)
 	{
